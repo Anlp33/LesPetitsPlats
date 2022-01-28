72b34e2c1a2d78f6724a53bbe3cc8cde374269db
Anne-Laure <annelaure.poire@gmail.com>
2022-01-28 17:46:41 +0100

    css error message corrections #2

--- scripts/pages/tag.js
+++ scripts/pages/tag.js
@@ -16,7 +16,7 @@ function closeTag() {
 
       //CSS style
       if (currentTag.length === 0) {
-        filters.style.top = "230px";
+        filters.style.top = "250px";
       }
 
       //faire la recherche principale en fonction de la barre de recherche
@@ -88,7 +88,7 @@ function addEventIngredientList() {
 
   itemIngredient.forEach((ingredient) => {
     ingredient.addEventListener("click", function (e) {
-      filters.style.top = "280px";
+      filters.style.top = "295px";
       const ingredientSearchBar = document.getElementById("ingrédients_input");
       ingredientSearchBar.value = "";
       itemClicked = e.target.innerHTML;
@@ -121,7 +121,7 @@ function addEventAppareilList() {
   const itemAppareil = document.querySelectorAll(".list_item_Appareils");
   itemAppareil.forEach((appareil) => {
     appareil.addEventListener("click", function (e) {
-      filters.style.top = "280px";
+      filters.style.top = "295px";
       const appareilSearchBar = document.getElementById("appareil_input");
       appareilSearchBar.value = "";
       itemClicked = e.target.innerHTML;
@@ -153,7 +153,7 @@ function addEventUstensilsList() {
   const itemUstensil = document.querySelectorAll(".list_item_Ustensils");
   itemUstensil.forEach((ustensil) => {
     ustensil.addEventListener("click", function (e) {
-      filters.style.top = "280px";
+      filters.style.top = "295px";
       const ustensilSearchBar = document.getElementById("ustensile_input");
       ustensilSearchBar.value = "";
       itemClicked = e.target.innerHTML;
